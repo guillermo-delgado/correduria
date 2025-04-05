1cc9489879a3cca87dae7f3499c5fe124a6d4cd7
Guillermo <aptoguilermo@gmail.com>
2025-04-05 23:38:09 +0200

    app1

--- src/hooks/usePwaUpdater.ts
+++ src/hooks/usePwaUpdater.ts
@@ -1,18 +1,16 @@
-// src/hooks/usePwaUpdater.ts
 import { useRegisterSW } from 'virtual:pwa-register/react';
-
 
 export function usePwaUpdater() {
   const {
     needRefresh,
-    updateServiceWorker
+    updateServiceWorker,
   } = useRegisterSW({
-    onRegistered(r) {
+    onRegistered(r: ServiceWorkerRegistration | undefined) {
       console.log('✅ Service Worker registrado:', r);
     },
-    onRegisterError(error) {
+    onRegisterError(error: unknown) {
       console.error('❌ Error registrando el Service Worker:', error);
-    }
+    },
   });
 
   return { needRefresh, updateServiceWorker };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module 'virtual:pwa-register/react' {
+    import type { SWUpdaterProps } from 'vite-plugin-pwa/client'
+  
+    export function useRegisterSW(
+      options?: SWUpdaterProps
+    ): {
+      needRefresh: boolean
+      updateServiceWorker: (reloadPage?: boolean) => void
+    }
+  }
+  
